28273d3b05a833e3f6a167bf8a27a60f8094fcdf
horsaen <atealltheglue@protonmail.com>
2022-09-30 16:44:27 -0500

    sync

--- server.js
+++ server.js
@@ -1,37 +1,26 @@
 const fs = require('fs')
 const axios = require('axios')
-const MeiliSearch = require('meilisearch')
 
 const links = [
-    { id: 'members', url: 'https://api.github.com/' },
-    { id: 'businesses', url: 'https://api.github.com/'}
+    {id:"name", url:"http://api.github.com"},
+    {id:"name2", url:"http://api.github.com"}
 ]
 
-links.map(getData, meiliSend)
+links.map(integration)
 
-function getData(item){
+function integration(item){
     axios
     .get(item.url)
     .then((res) => {
-        fs.writeFile('./data/' + item.url, JSON.stringify(res.data), err => {
+        fs.writeFile('./data/' + item.id + '.json', JSON.stringify(res.data), err => {
             if(err) {
                 console.log(err)
             } else {
-                console.log('success')
+                console.log('Successfully written "' + item.id + '" to /data/' + item.id + '.json')
             }
         })
-        meiliSend()
     })
-    .catch((err) => {
-        console.log(err)
+    .then(() => {
+        console.log(item.id)
     })
 }
-
-function meiliSend(item){
-    const documents = require('./data/' + item.name + '.json')
-    const client = new MeiliSearch({ host: 'http://127.0.0.1:7700', apiKey:  'xxxx'})
-    client.index(item.name).deleteAllDocuments()
-    client.index(item.name).addDocuments(documents)
-}
-fetchData()
-setInterval(fetchData,  60000)
