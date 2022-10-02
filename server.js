cca30354b85f5bcabddd3b29c978b3534cef0ef1
horsaen <atealltheglue@protonmail.com>
2022-10-02 17:10:43 -0500

    meili integration

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
 const fs = require('fs')
 const axios = require('axios')
+const { MeiliSearch } = require('meilisearch')
 
 const links = [
     {id:"name", url:"http://api.github.com"},
     {id:"name2", url:"http://api.github.com"}
 ]
 
+const key = 'xxxx'
+
 links.map(integration)
 
-function integration(item){
+function integration(api){
     axios
-    .get(item.url)
+    .get(api.url)
     .then((res) => {
-        fs.writeFile('./data/' + item.id + '.json', JSON.stringify(res.data), err => {
+        fs.writeFile('./data/' + api.id + '.json', JSON.stringify(res.data), err => {
             if(err) {
                 console.log(err)
             } else {
-                console.log('Successfully written "' + item.id + '" to /data/' + item.id + '.json')
+                console.log('Successfully written "' + api.id + '" to /data/' + api.id + '.json')
             }
         })
     })
     .then(() => {
-        console.log(item.id)
+        const client = new MeiliSearch({ host: 'http://127.0.0.1:7700', apiKey: key,})
+        client.index(api.id).deleteAllDocuments()
+        client.index(api.id).addDocuments('./data/' + api.id + '.json')
     })
 }
