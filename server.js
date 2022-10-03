339083fb989899e0ce2f99030a20819d89cf4c61
horsaen <atealltheglue@protonmail.com>
2022-10-02 22:47:45 -0500

    working integration

--- server.js
+++ server.js
@@ -3,32 +3,34 @@ const axios = require('axios')
 const { MeiliSearch } = require('meilisearch')
 
 const links = [
-    {id:"name", url:"http://api.github.com"},
-    {id:"name2", url:"http://api.github.com"}
+  {id:"abouts",url:"http://127.0.0.1:5000/about"},
+  {id:"businesses",url:"http://127.0.0.1:5000/business"},
+  {id:"members",url:"http://127.0.0.1:5000/members"}
 ]
-
-const key = 'xxxx'
 
 links.map(integration)
 
 function integration(api){
+  const getData = () => {
     axios
     .get(api.url)
-    .then((res) => {
-        fs.writeFile('./data/' + api.id + '.json', JSON.stringify(res.data), err => {
-            if(err) {
-                console.log(err)
-            } else {
-                console.log('Successfully written "' + api.id + '" to /data/' + api.id + '.json')
-            }
-        })
+    .then((res) =>{
+      fs.writeFile('./data/' + api.id + '.json', JSON.stringify(res.data), err => {
+        if(err) {
+          console.log(err)
+        } else {
+          console.log('Written "' + api.id + '" to ./data/' + api.id + '.json')
+        }
+      })
     })
-    .then(() => {
-        const client = new MeiliSearch({ host: 'http://127.0.0.1:7700', apiKey: key,})
-        client.index(api.id).deleteAllDocuments()
-        client.index(api.id).addDocuments('./data/' + api.id + '.json')
-    })
-    .catch((err) => {
-        console.log(err)
-    })
+  }
+  const sendData = () => {
+    const data = './data/' + api.id + '.json'
+    const files = require(data)
+    const client = new MeiliSearch({ host: 'http://localhost:7700', apiKey: 'xxxx' })
+    client.index(api.id).addDocuments(files)
+    .then((res) => console.log(res))
+  }
+  getData()
+  sendData()
 }
